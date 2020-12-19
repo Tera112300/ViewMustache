e59eae5e17b6184101def7457ed368814cefc2eb
Tera112300 <terao112300@gmail.com>
2020-12-19 23:50:54 +0900

    no message

--- views/index_test.view.php
+++
@@ -1,8 +0,0 @@
-<h2>Hello Index.php {{ $title }}</h2>
-
-{{ require 'layout/dummy' }}
-{{ require 'layout/base' }}
-
-<h2>{{ $title }}</h2>
-{{helper('test02',$value,'tesr')}}<br>
-{{helper('toOsaka',$value)}}tesf
